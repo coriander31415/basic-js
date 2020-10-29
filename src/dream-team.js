1356937a4001db732c2591b89c44ecb67f19babc
coriander31415 <coriander31415@gmail.com>
2020-10-29 12:24:04 +0200

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+    return (!Array.isArray(members)) ? false : members
+        .filter(name => typeof(name) === 'string')
+        .map(name => name
+            .trim()[0]
+            .toUpperCase())
+        .sort()
+        .join('');
+
 };
+
+// check if members is array
+// check if members' elements are string 
+// reduce whitespaces!
+// get and capitalize first letters of each name
