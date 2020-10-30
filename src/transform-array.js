e60b90b35757c68d7ce1a35426bd74220d8589a7
coriander31415 <coriander31415@gmail.com>
2020-10-30 12:14:17 +0200

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                if (res.length !== 0 && arr[i - 2] !== '--discard-next') {
+                    res.pop();
+                }
+                break;
+            case '--double-next':
+                if (i < arr.length - 1) {
+                    res.push(arr[i + 1]);
+                }
+                break;
+            case '--double-prev':
+                if (i && arr[i - 2] !== '--discard-next') {
+                    res.push(arr[i - 1]);
+                }
+                break;
+            default:
+                res.push(arr[i]);
+                break;
+        }
+    }
+    return res;
 };
